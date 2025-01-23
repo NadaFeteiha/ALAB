b6c09198e2606889dc5bfe298d1ff5497117364e
Nada Feteiha <nada.feteiha@gmail.com>
2025-01-23 06:39:48 -0800

    add lab 308.5.1

---
+++ ALAB 308.5.1/index.js
@@ -0,0 +1,2 @@
+// https://www.canva.com/design/DAFxu0_85EM/7WLacHOPNDMOl8GnhbdatQ/edit
+// ALAB 308.5.1: Creating Reusable Functions
--- ALAB-308.4.1/index.js
+++ ALAB-308.4.1/index.js
@@ -1,6 +1,29 @@
 //ALAB 308.4.1:
 //TODO: Part 1: Refactoring Old Code
 
+let csv= "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+let cells =[] // 4  6
+let str =""
+let rowsNum =0
+let cellsNum = 0
+for (let i=0; i<=csv.length; i++){//i=5
+    let char = csv[i]; // ,
+    if(char===","){
+        cells.push(str) //Index
+        str = ""// empty
+    } else if (char==="\n" || i===csv.length){
+        rowsNum++;
+        cells.push(str)// str = Spring 2 (m)
+        console.log(cells)
+        cellsNum = cells.length
+        str=""
+        cells =[]
+    } else {
+        str+= char //
+    }
+}
+console.log(`cells ${cellsNum} per rows  ${cellsNum*rowsNum} rows =${rowsNum}`)
+
 let csvString = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
 // using helper functions and list..
