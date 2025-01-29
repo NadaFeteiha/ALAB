7e4c55152cf148eb2717509a2fc83e7605b79347
Nada Feteiha <nada.feteiha@gmail.com>
2025-01-29 12:13:06 -0800

    solve part-2

--- ALAB-316.1.1/index.js
+++ ALAB-316.1.1/index.js
@@ -14,5 +14,19 @@ mainEl.appendChild(h1El);
 //TODO: 4.Add a class of flex-ctr to mainEl.
 mainEl.classList.add("flex-ctr");
 
+//========================================================================================================
+// Part 2: Creating a Menu Bar
+//TODO: 1.Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.getElementById("top-menu");
+
+//TODO: 2.Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = "100%";
+
+//TODO: 3.Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+//TODO: 4.Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add("flex-around")
+
 
 
