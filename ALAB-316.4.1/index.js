8f2c3f39b5206c62321f551b84413883ed0d2773
Nada Feteiha <nada.feteiha@gmail.com>
2025-02-05 07:49:20 -0800

    add required fields in html

---
+++ ALAB-316.4.1/index.js
@@ -0,0 +1,13 @@
+// ALAB 316.4.1: Form Validation
+
+
+// Part 1: Introduction
+// Your job is to add validation so that the forms adhere to the requirements outlined below.
+// You can make changes to the HTML (and CSS)
+// An HTML element with id errorDisplay has been provided as 
+// a convenient method of showing error text to the user. 
+// In order to show or hide errorDisplay, you must modify its display style attribute.
+// You can place any text or HTML into errorDisplay.
+
+
+
