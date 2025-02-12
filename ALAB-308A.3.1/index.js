cca194ac789e30c316c1038f8427642e825b8f3d
Nada Feteiha <nada.feteiha@gmail.com>
2025-02-12 12:59:41 -0800

    get DB name for the given id

--- ALAB-308A.3.1/index.js
+++ ALAB-308A.3.1/index.js
@@ -1,18 +1,24 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
+
+console.log("================ central ====================")
 // central: database identifies which database the users are stored within
 const val = await central(1);
 console.log(val); // returns-> db1
 
+
+console.log("================ db1 ====================")
 // db1, db2. db3: databases contain the user's basic information, including username, website, and company.
 const val2 = await db1(1)
 console.log(val2);
 
+console.log("================ vault ====================")
 // val: The personal data for each user is contained within the vault database since its access and usage is restricted by law.
 const val3 = await vault(1);
 console.log(val3);
 
+console.log("============================================")
 
 // todo: solve by =>    Promise chaining via then() statements
 // todo: then =>        async/await syntax
@@ -35,6 +41,13 @@ function getUserData(id) {
         db3: db3
     };
 
+    // Step 1: Get db name
+    return central(id)
+        .then(dbName => {
+            // Get basic user info
+            console.log(`HERE DB Name := ${dbName}`);
+            
+        })
 }
 
 
@@ -45,3 +58,5 @@ function getUserData(id) {
 //!     3. Invalid data types – strings, Booleans, etc.
 
 
+let user = await getUserData(1);
+console.log(user);
