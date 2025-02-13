e1e16621e985071ab6d372bb0993f24cf46a1923
Nada Feteiha <nada.feteiha@gmail.com>
2025-02-13 10:00:01 -0800

    refactor

--- ALAB-308A.3.1/index.js
+++ ALAB-308A.3.1/index.js
@@ -36,14 +36,10 @@ console.log("===================================================================
 
 
 function getUserData(id) {
-    const dbs = {
-        db1: db1,
-        db2: db2,
-        db3: db3
-    };
+    const dbs = { db1: db1, db2: db2, db3: db3 };
 
     // Step 1: Get db name
-    return central(id)
+    const result = central(id)
         .then(dbName => {
             console.log(`HERE DB Name := ${dbName}`);
             return Promise.all([id, dbs[dbName](id), vault(id)]);
@@ -60,6 +56,8 @@ function getUserData(id) {
         }).catch(e => {
             throw new Error(`Error: ${e}`);
         });
+        
+    return result;
 }
 
 
@@ -72,60 +70,29 @@ function getUserData(id) {
 // test case 1: Valid user id
 
 let user = getUserData(1)
-    .then(data => {
-        console.log(`################# User id = 1 #################`);
-        console.log(data);
-        console.log(`##################################`);
-    });
-
+    .then(data => resoledPromise(1, data))
+    .catch(e => rejectedPromise(1, e));
 
 let user3 = getUserData(3)
-    .then(data => {
-        console.log(`################# User id = 3 #################`);
-        console.log(data);
-        console.log(`######################################`);
-    }).catch(e => {
-        console.log(e);
-    });
+    .then(data => resoledPromise(3, data))
+    .catch(e => rejectedPromise(3, e));
 
 let user10 = getUserData(10)
-    .then(data => {
-        console.log(`################# User id = 10 #################`);
-        console.log(data);
-        console.log(`######################################`);
-    }).catch(e => {
-        console.log(e);
-    });
+    .then(data => resoledPromise(10, data))
+    .catch(e => rejectedPromise(10, e));
 
 // test case 2: Invalid user id
 let user0 = getUserData(0)
-    .then(data => {
-        console.log(`################# User id = 0 #################`);
-        console.log(data);
-        console.log(`######################################`);
-    })
-    .catch(e => {
-        console.log(e);
-    });
-
+    .then(data => resoledPromise(0, data))
+    .catch(e => rejectedPromise(0, e));
 
 let userN = getUserData(-1)
-    .then(data => {
-        console.log(data);
-    })
-    .catch(e => {
-        console.log(`################# User id = -1 #################`);
-        console.log(e);
-    });
+    .then(data => resoledPromise(-1, data))
+    .catch(e => rejectedPromise(-1, e));
 
 let user20 = getUserData(20)
-    .then(data => {
-        console.log(data);
-    })
-    .catch(e => {
-        console.log(`################# User id = 20 #################`);
-        console.log(e);
-    });
+    .then(data => resoledPromise(20, data))
+    .catch(e => rejectedPromise(20, e));
 
 // test case 3: Invalid data types
 let userS = getUserData('S')
@@ -135,3 +102,17 @@ let userS = getUserData('S')
         console.log(`################# User id = S #################`);
         console.log(e);
     });
+
+
+// Helper functions
+function resoledPromise(id, data) {
+    console.log(`################# User id = ${id} #################`);
+    console.log(data);
+    console.log(`##################################`);
+}
+
+function rejectedPromise(id, error) {
+    console.log(`################# User id = ${id} #################`);
+    console.log(error);
+    console.log(`##################################`);
+}
