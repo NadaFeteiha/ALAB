411f95120a96d8b8fc82bff97224571b3e80ff7f
Nada Feteiha <nada.feteiha@gmail.com>
2025-02-14 20:35:39 -0800

    add part 4

--- RALAB-308A.4.1/axios-script.js
+++ RALAB-308A.4.1/axios-script.js
@@ -4,6 +4,25 @@ import { API_KEY as CAT_API_KEY } from "./config.js";
 /**
  * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
  */
+
+/**
+ * 4. Change all of your fetch() functions to axios!
+ * - axios has already been imported for you within index.js.
+ * - If you've done everything correctly up to this point, this should be simple.
+ * - If it is not simple, take a moment to re-evaluate your original code.
+ * - Hint: Axios has the ability to set default headers. Use this to your advantage
+ *   by setting a default header with your API key so that you do not have to
+ *   send it manually with all of your requests! You can also set a default base URL!
+ */
+
+//TODO not done yet 
+/**
+ * 5. Add axios interceptors to log the time between request and response to the console.
+ * - Hint: you already have access to code that does this!
+ * - Add a console.log statement to indicate when requests begin.
+ * - As an added challenge, try to do this on your own without referencing the lesson material.
+ */
+
 const API_KEY = CAT_API_KEY;
 const BASE_URL = "https://api.thecatapi.com/v1";
 
