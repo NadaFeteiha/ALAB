89970c66c5d4532acdcbadb01561f679971daa55
Nada Feteiha <nada.feteiha@gmail.com>
2025-02-28 11:38:18 -0800

    add Score compnent

--- RALAB-320H.3.1/rendering-arrays/src/components/LearnerCard.jsx
+++ RALAB-320H.3.1/rendering-arrays/src/components/LearnerCard.jsx
@@ -1,10 +1,11 @@
-
+import Score from './Score';
 
 function LearnerCard({ learner }) {
     return (
         <div className="learner-card">
             <h2>{learner.name}</h2>
             <p>{learner.bio}</p>
+            <Score />
         </div>
     );
 }
--- RALAB-320H.3.1/rendering-arrays/src/components/Learners.jsx
+++ RALAB-320H.3.1/rendering-arrays/src/components/Learners.jsx
@@ -1,9 +1,10 @@
 import LearnerCard from "./LearnerCard";
+import styles from './Learners.module.css';
 
 
 export default function Learners({ learnerList }) {
     return (
-        <div>
+        <div className={styles.container}>
             {
                 learnerList.map((learner) => (
                     <LearnerCard key={learner.id} learner={learner} />
