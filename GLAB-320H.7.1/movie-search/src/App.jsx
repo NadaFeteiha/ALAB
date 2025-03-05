ea7addc86cd661bb1b2e60bb1eda6d1cb8b37730
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-05 09:02:44 -0800

    fix the behavior

--- GLAB-320H.7.1/movie-search/src/App.jsx
+++ GLAB-320H.7.1/movie-search/src/App.jsx
@@ -4,55 +4,54 @@ import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form"
 import RandomNumber from './data/RandomNumber';
 import Loading from './components/Loading';
-
+import movies from './data/movisIds.js'
 const BASE_URL = "http://www.omdbapi.com/?";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
   const [movie, setMovie] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const getMovie = async (searchTerm) => {
     setLoading(true);
 
     try {
       const response = await fetch(`${BASE_URL}apiKey=${API_KEY}&t=${searchTerm}`);
-      const data = await response.json();      
-
-      setInterval(() => {
-        setLoading(false);
-        setMovie(data);
-      }, 1000);
-
+      const data = await response.json();
+      console.log(`here=> ${data}`)
+      setMovie(data);
     } catch (e) {
       console.error(e)
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    //tt120 1607
-    const id = `tt1201607`//${RandomNumber()}${RandomNumber()};
-    console.log(id);
+    const index = RandomNumber(0, movies.length - 1);
+    console.log(`index=> ${index}`);
+    const id = movies[index];
 
     (async () => {
       try {
         const response = await fetch(`${BASE_URL}apiKey=${API_KEY}&i=${id}`);
         const data = await response.json();
-        console.log(`**************** ${data.Title} ****************`);
         setLoading(false);
         setMovie(data);
       } catch (e) {
         console.error(e)
       }
     })();
-
   }, []);
 
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
-      <Loading isLoading={loading} />
-      <MovieDisplay movie={movie} isLoading={loading} />
+      {
+        loading && <Loading />}
+      {
+        movie && <MovieDisplay movie={movie} />
+      }
     </div>
   )
 }
