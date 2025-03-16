78057fd3405fc028f2260aa175d77788d4ba01b0
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-16 01:30:56 -0700

    add the routes

---
+++ SBA_318/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(morgan("morgan"));
+app.use(express.static("public"));
+
+// Routes
+const applicationRoutes = require("./routes/applications");
+const companyRoutes = require("./routes/companies");
+
+app.use("/applications", applicationRoutes);
+app.use("/companies", companyRoutes);
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ error: err.message });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
